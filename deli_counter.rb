efb7a1138d21034ea4fb2bb485db0a4aa6df3bcf
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 17:09:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,6 @@
 # Write your code here.
 def line(deli)
-  name = 0
-  if deli != []
-    deli.each { |name| puts "The line is currently: #{name}" }
-  else
     puts "The line is currently empty."
-  end
 end
 
 def take_a_number
