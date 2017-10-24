dff146c59f16ddf3b7148e5ab6a9120eac7aed1b
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 16:30:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli == 0
+  if katz_deli != 0
+    puts "The line is currently: #{katz_deli}"
+  else
     puts "The line is currently empty."
  end
 end
