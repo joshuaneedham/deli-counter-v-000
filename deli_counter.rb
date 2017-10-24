a6b17bc575e7ce63e45ec4fe3ec0e02d1d7f386f
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 17:28:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,19 @@
 def line(deli_line)
   if deli_line != []
     new_line = deli_line.map.with_index { |name, index| "#{index + 1}. #{name}" }
-    puts "The line is currently: #{new_line.join(" ")}"
+    puts "Welcome, #{name}. You are number #{index} in line."
   else
     puts "The line is currently empty."
   end
 end
 
 def take_a_number(deli_line, name)
-  
+  if deli_line != []
+    new_line = deli_line.map.with_index { |name, index| "#{name}" "#{index + 1}" }
+    puts "The line is currently: #{new_line.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
 end
 
 def now_serving
