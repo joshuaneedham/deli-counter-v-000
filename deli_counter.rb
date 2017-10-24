0a720d4b021de64b0e58bb63c8472937a06b638e
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 17:20:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 def line(deli_line)
   if deli_line != []
-    deli_line.each_with_index { |name, index| "#{index + 1} #{name}" }
-    puts "The line is currently: #{deli_line}"
+    new_line = deli_line.map.with_index { |name, index| "#{index + 1}. #{name}" }
+    puts "The line is currently: #{new_line}"
   else
     puts "The line is currently empty."
   end
